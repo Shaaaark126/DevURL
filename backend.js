c5b36012d178d3eaaede91c7539a36a29d8eefae
Shaaaark126 <96465389+Shaaaark126@users.noreply.github.com>
2024-02-11 12:08:37 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -12,12 +12,11 @@ const communityCoordinates = require('./latlongs.js')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var gotstring = ""
-var code = ""
 var area = ""
 
 let thetext
 let crimeObject
+
 const genAI = new GoogleGenerativeAI(process.env.GAPI_KEY);
 
 async function gemini(prompt) {
@@ -27,54 +26,30 @@ async function gemini(prompt) {
   const result = await model.generateContent(prompt);
   const response = await result.response;
   thetext = response.text();
-    console.log(thetext);
-}
-
-
-// Function to retrieve latitude and longitude for a given area name
-function getCoordinates(area) {
-  const coordinates = communityCoordinates[area];
-  if (coordinates) {
-    return coordinates; // Return an object with latitude and longitude
-  } else {
-    return null; // Return null if areaName doesn't exist in the object
-  }
 }
 
 app.post('/submit-area-code', async (req, res) => {
 
-    gotstring = req.body.area
-    code = gotstring.slice(0,2)
+    var gotstring = req.body.area
     area = gotstring.slice(3)
-    console.log(code," and ", area)
-
-    const coordinates = getCoordinates(area);
-  if (coordinates) {
-    console.log(`Latitude: ${coordinates.latitude}, Longitude: ${coordinates.longitude}`);
-  } 
-  else {
-  console.log(`No coordinates found for ${area}`);
-  }
 
   var theprompt = `Among all other community districts in Chicago, give a percentage possibility of crime in ${area} Give output like this Possibility of crime:(answer)  Most Likely type of crime to occur(only one answer)`;
 
-  
     console.log(theprompt)
   
-    await gemini(theprompt)
-
+    await gemini(theprompt) 
   
     let possibilityIndex = thetext.indexOf("Possibility of crime: ") + "Possibility of crime: ".length;
     let typeIndex = thetext.indexOf("Most Likely type of crime to occur: ") + "Most Likely type of crime to occur: ".length;
     
-    // Extracting data
     let possibilitySubstring = thetext.substring(possibilityIndex, thetext.indexOf("%", possibilityIndex));
     let typeSubstring = thetext.substring(typeIndex);
 
     crimeObject = {
-      possibility: parseFloat(possibilitySubstring), // Convert possibility to a float
-      type: typeSubstring.trim(), // Remove leading/trailing spaces
-      ttext: thetext
+      possibility: parseFloat(possibilitySubstring), 
+      type: typeSubstring.trim(), 
+      ttext: thetext,
+      tarea: area
   };
 
 console.log(crimeObject.possibility)
@@ -83,43 +58,26 @@ console.log(crimeObject.type)
   res.redirect('/result')
 });
 
-/*async function findPoliceStations() {
-  try {
-    const response = await axios.post('https://places.googleapis.com/v1/places:searchNearby', {
-      includedTypes: ['police'],
-      maxResultCount: 10,
-      locationRestriction: {
-        circle: {
-          center: {
-            latitude: 19.2183,
-            longitude: 72.9781
-          },
-          radius: 10000.0
-        }
-      }
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Goog-Api-Key': process.env.GAPI_KEY,
-        'X-Goog-FieldMask': 'places.displayName'
-      }
-    });
-
-    // Handle response
-    console.log('Police Stations near',);
-    response.data.places.forEach(place => {
-      console.log(place.displayName);
-    });
-  } catch (error) {
-    console.error('Error fetching coffee shops:', error.message);
-  }
-}
-
-findPoliceStations();*/
-
 app.get('/result', (req,res) => {
 
   res.render('result', { crimeObject });
+})
+
+app.get('/location',(req,res) => {
+
+  res.render('geo.ejs');
+
+})
+
+app.get('/table',(req,res) => {
+
+  res.render('table.ejs');
+
+})
+
+app.get('/outreach', (req,res) => {
+
+  res.render("outreach.ejs");
 })
 
 app.get('/', (req,res) => {
