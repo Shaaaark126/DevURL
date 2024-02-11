fb99ee2b909e32ab5d2664aea27a0b373bdb28a7
Shaaaark126 <96465389+Shaaaark126@users.noreply.github.com>
2024-02-11 12:11:25 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,48 +1,54 @@
-let map;
-let service;
-let infowindow;
+function loadlocation(){
 
-/*function initMap() {  
-  const thane = new google.maps.LatLng(19.2183, 72.9781);
-
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: thane,
-    zoom: 15,
-  });
-
-  const request = {
-    query: "Viviana Mall, Thane",
-    fields: ["name", "geometry"],
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+    fetch('/location')
+        .then(response => {
+        
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            window.location.href = response.url;
+        })
+        .catch(error => {
+            
+            console.error('Error fetching data:', error);
+        });
 }
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
+function loadleaderboard(){
 
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
+    fetch('/table')
+        .then(response => {
+        
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            window.location.href = response.url;
+        })
+        .catch(error => {
+            
+            console.error('Error fetching data:', error);
+        });
 }
 
-window.initMap = initMap;*/
+function loadoutreach(){
 
-// Cdde to put marker on a map above
+    fetch('/outreach')
+        .then(response => {
+        
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            window.location.href = response.url;
+        })
+        .catch(error => {
+            
+            console.error('Error fetching data:', error);
+        });
+}
+
+
+
 
